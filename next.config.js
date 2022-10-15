c121734207d414279e3a82a8f772bd9d54191909
Feng Kaiyu <loveress01@outlook.com>
2022-10-15 20:20:13 +0800

    refactor: remove dependency 'next-compose-plugins'

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 
-const withPlugins = require('next-compose-plugins')
+// const withPlugins = require('next-compose-plugins')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports = withPlugins([[withBundleAnalyzer]], {
+module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
   images: {
     domains: [
