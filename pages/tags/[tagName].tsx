25fc3a13a46ac1c3ae241d4d8ce29afa27fe1956
yuriportfolio <37544475+yuriportfolio@users.noreply.github.com>
2023-02-19 23:09:49 +0000

    super tag page

--- pages/tags/[tagName].tsx
+++ pages/tags/[tagName].tsx
@@ -1,12 +1,30 @@
 import React from 'react'
-
 import { domain, isDev, rootNotionPageId } from 'lib/config'
 import { resolveNotionPage } from 'lib/resolve-notion-page'
 import omit from 'lodash.omit'
 import { ExtendedRecordMap } from 'notion-types'
 import { normalizeTitle } from 'notion-utils'
-
 import { NotionPage } from '@/components/NotionPage'
+import { NUMBER_OF_POSTS_PER_PAGE } from '../../app/server-constants'
+import GoogleAnalytics from '../../components/google-analytics'
+import {
+  BlogPostLink,
+  BlogTagLink,
+  NextPageLink,
+  NoContents,
+  PostDate,
+  PostExcerpt,
+  PostTags,
+  PostTitle,
+  ReadMoreLink,
+} from '../../components/blog-parts'
+import styles from '../../styles/blog.module.css'
+import {
+  getPosts,
+  getFirstPost,
+  getRankedPosts,
+  getAllTags,
+} from '../../lib/notion/client'
 
 const tagsPropertyNameLowerCase = 'tags'
 
@@ -84,10 +102,21 @@ export const getStaticProps = async (context) => {
       }
     }
 
+    const [posts, firstPost, rankedPosts, tags] = await Promise.all([
+      getPosts(NUMBER_OF_POSTS_PER_PAGE),
+      getFirstPost(),
+      getRankedPosts(),
+      getAllTags(),
+    ])
+
     return {
       props: {
         ...props,
         tagsPage: true,
+        posts,
+        firstPost,
+        rankedPosts,
+        tags,
         propertyToFilterName
       },
       revalidate: 43200
@@ -134,6 +163,46 @@ export async function getStaticPaths() {
   }
 }
 
+const BlogPageTag = async ({ posts, firstPost, rankedPosts, tags }) => {
+  return (
+    <>
+      <GoogleAnalytics pageTitle="Blog" />
+      <div className={styles.container}>
+        <div className={styles.mainContent}>
+          <NoContents contents={posts} />
+
+          {posts.map(post => {
+            return (
+              <div className={styles.post} key={post.Slug}>
+                <PostDate post={post} />
+                <PostTags post={post} />
+                <PostTitle post={post} />
+                <PostExcerpt post={post} />
+                <ReadMoreLink post={post} />
+              </div>
+            )
+          })}
+
+          <footer>
+            <NextPageLink firstPost={firstPost} posts={posts} />
+          </footer>
+        </div>
+
+        <div className={styles.subContent}>
+          <BlogPostLink heading="Recommended" posts={rankedPosts} />
+          <BlogTagLink heading="Categories" tags={tags} />
+        </div>
+      </div>
+    </>
+  )
+}
+
 export default function NotionTagsPage(props) {
+  const { tagsPage, propertyToFilterName, posts, firstPost, rankedPosts, tags } = props
+
+  if (tagsPage) {
+    return <BlogPageTag posts={posts} firstPost={firstPost} rankedPosts={rankedPosts} tags={tags} />
+  }
+
   return <NotionPage {...props} />
 }
