655f51c1c4f3e6372b8bf57d0c04676219b3c24b
yuriportfolio <37544475+yuriportfolio@users.noreply.github.com>
2023-02-20 00:45:38 +0000

    great fix with routers

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,110 +1,141 @@
-import Link from 'next/link'
-import * as React from 'react'
-import { FaEnvelopeOpenText } from '@react-icons/all-files/fa/FaEnvelopeOpenText'
-import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
-import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
-import { FaMastodon } from '@react-icons/all-files/fa/FaMastodon'
-import { FaRss } from '@react-icons/all-files/fa/FaRss'
-import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter'
-import { FaYoutube } from '@react-icons/all-files/fa/FaYoutube'
-import { FaZhihu } from '@react-icons/all-files/fa/FaZhihu'
-import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
-import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+import * as React from 'react';		
 
-import * as config from '@/lib/config'
-import { useDarkMode } from '@/lib/use-dark-mode'
+import { FaEnvelopeOpenText } from '@react-icons/all-files/fa/FaEnvelopeOpenText';		
+import { FaGithub } from '@react-icons/all-files/fa/FaGithub';		
+import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin';		
+import { FaMastodon } from '@react-icons/all-files/fa/FaMastodon';		
+import { FaRss } from '@react-icons/all-files/fa/FaRss';		
+import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter';		
+import { FaYoutube } from '@react-icons/all-files/fa/FaYoutube';		
+import { FaZhihu } from '@react-icons/all-files/fa/FaZhihu';		
+import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp';		
+import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline';		
 
-import styles from './styles.module.css'
+import * as config from '@/lib/config';		
+import { useDarkMode } from '@/lib/use-dark-mode';		
 
-const year = new Date().getFullYear()
+import styles from './styles.module.css';		
 
-const Footer = () => {
-  const { isDarkMode, onToggleDarkMode } = useDarkMode()
+const year = new Date().getFullYear();		
 
-  return (
-    <footer className={styles.footer}>
-      <div>
-        <span>Powered by </span>
-        <Link href="https://github.com/otoyo/easy-notion-blog">
-          easy-notion-blog
-        </Link>
-      </div>
-      <div className={styles.footer}>
-        <div className={styles.settings}>
-          <a
-            className={styles.toggleDarkMode}
-            href="#"
-            role="button"
-            onClick={onToggleDarkMode}
-            title="Toggle dark mode"
-          >
-            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
-          </a>
-        </div>
+const Footer: React.FC = () => {		
+  const [hasMounted, setHasMounted] = React.useState(false);		
+  const { isDarkMode, toggleDarkMode } = useDarkMode();		
 
-        <div className={styles.social}>
-          <a
-            className={styles.rss}
-            href="/feed.xml"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaRss />
-          </a>
-          {config.twitter && (
-            <a
-              className={styles.twitter}
-              href={`https://twitter.com/${config.twitter}`}
-              title={`Twitter @${config.twitter}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaTwitter />
-            </a>
-          )}
+  const onToggleDarkMode = React.useCallback(		
+    (e) => {		
+      e.preventDefault();		
+      toggleDarkMode();		
+    },		
+    [toggleDarkMode]		
+  );		
 
-          {config.mastodon && (
-            <a
-              className={styles.mastodon}
-              href={config.mastodon}
-              title={`Mastodon ${config.getMastodonHandle()}`}
-              rel="me"
-            >
-              <FaMastodon />
-            </a>
-          )}
+  React.useEffect(() => {		
+    setHasMounted(true);		
+  }, []);		
 
-          {config.zhihu && (
-            <a
-              className={styles.zhihu}
-              href={`https://zhihu.com/people/${config.zhihu}`}
-              title={`Zhihu @${config.zhihu}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaZhihu />
-            </a>
-          )}
+  return (		
+    <footer className={styles.footer}>		
+      <div className={styles.copyright}>		
+        Copyright {year} {config.author}		
+      </div>		
 
-          {config.github && (
-            <a
+      <div className={styles.settings}>		
+        {hasMounted && (		
+          <a		
+            className={styles.toggleDarkMode}		
+            href="#"		
+            role="button"		
+            onClick={onToggleDarkMode}		
+            title="Toggle dark mode"		
+          >		
+            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}		
+          </a>		
+        )}		
+      </div>		
 
-            className={styles.github}
-            href={`https://github.com/${config.github}`}
-            title={`GitHub @${config.github}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithub />
-          </a>
-        )}
+      <div className={styles.social}>		
+        <a		
+          className={styles.rss}		
+          href="/feed.xml"		
+          target="_blank"		
+          rel="noopener noreferrer"		
+        >		
+          <FaRss />		
+        </a>		
+        {config.twitter && (		
+          <a		
+            className={styles.twitter}		
+            href={`https://twitter.com/${config.twitter}`}		
+            title={`Twitter @${config.twitter}`}		
+            target="_blank"		
+            rel="noopener noreferrer"		
+          >		
+            <FaTwitter />		
+          </a>		
+        )}		
 
-        <Link href="/about">
-          <a className={styles.about}>About</a>
-        </Link>
-      </div>
-    </div>
-  </footer>
-)}
+        {config.mastodon && (		
+          <a		
+            className={styles.mastodon}		
+            href={config.mastodon}		
+            title={`Mastodon ${config.getMastodonHandle()}`}		
+            rel="me"		
+          >		
+            <FaMastodon />		
+          </a>		
+    )}
 
-export default Footer
+    {config.github && (		
+      <a		
+        className={styles.github}		
+        href={`https://github.com/${config.github}`}		
+        title={`GitHub @${config.github}`}		
+        target="_blank"		
+        rel="noopener noreferrer"		
+      >		
+        <FaGithub />		
+      </a>		
+    )}		
+
+    {config.linkedin && (		
+      <a		
+        className={styles.linkedin}		
+        href={`https://www.linkedin.com/in/${config.linkedin}`}		
+        title={`LinkedIn ${config.author}`}		
+        target="_blank"		
+        rel="noopener noreferrer"		
+      >		
+        <FaLinkedin />		
+      </a>		
+    )}		
+
+    {config.newsletter && (		
+      <a		
+        className={styles.newsletter}		
+        href={`${config.newsletter}`}		
+        title={`Newsletter ${config.author}`}		
+        target="_blank"		
+        rel="noopener noreferrer"		
+      >		
+        <FaEnvelopeOpenText />		
+      </a>		
+    )}		
+
+    {config.youtube && (		
+      <a		
+        className={styles.youtube}		
+        href={`https://www.youtube.com/${config.youtube}`}		
+        title={`YouTube ${config.author}`}		
+        target="_blank"		
+        rel="noopener noreferrer"		
+      >		
+        <FaYoutube />		
+      </a>		
+    )}		
+  </div>		
+</footer>		
+);
+};
+
+export default Footer;
