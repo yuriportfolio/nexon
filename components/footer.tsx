102bbad5173bf245365199953cbb9749b4cbaa18
yuriportfolio <37544475+yuriportfolio@users.noreply.github.com>
2023-02-20 01:19:18 +0000

    Footer revert

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,141 +1,15 @@
-import React from 'react'
+import Link from 'next/link'
 
-import { FaEnvelopeOpenText } from '@react-icons/all-files/fa/FaEnvelopeOpenText';		
-import { FaGithub } from '@react-icons/all-files/fa/FaGithub';		
-import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin';		
-import { FaMastodon } from '@react-icons/all-files/fa/FaMastodon';		
-import { FaRss } from '@react-icons/all-files/fa/FaRss';		
-import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter';		
-import { FaYoutube } from '@react-icons/all-files/fa/FaYoutube';		
-import { FaZhihu } from '@react-icons/all-files/fa/FaZhihu';		
-import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp';		
-import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline';		
+import styles from '../styles/footer.module.css'
 
-import * as config from '@/lib/config';		
-import { useDarkMode } from '@/lib/use-dark-mode';		
+const Footer = () => (
+  <footer className={styles.footer}>
+    <div>
+      <span>Powered by </span>
+      <Link href="https://github.com/otoyo/yuriportfolio">
+Yuri      </Link>
+    </div>
+  </footer>
+)
 
-import styles from './styles.module.css';		
-
-const year = new Date().getFullYear();		
-
-const Footer: React.FC = () => {		
-  const [hasMounted, setHasMounted] = React.useState(false);		
-  const { isDarkMode, toggleDarkMode } = useDarkMode();		
-
-  const onToggleDarkMode = React.useCallback(		
-    (e) => {		
-      e.preventDefault();		
-      toggleDarkMode();		
-    },		
-    [toggleDarkMode]		
-  );		
-
-  React.useEffect(() => {		
-    setHasMounted(true);		
-  }, []);		
-
-  return (		
-    <footer className={styles.footer}>		
-      <div className={styles.copyright}>		
-        Copyright {year} {config.author}		
-      </div>		
-
-      <div className={styles.settings}>		
-        {hasMounted && (		
-          <a		
-            className={styles.toggleDarkMode}		
-            href="#"		
-            role="button"		
-            onClick={onToggleDarkMode}		
-            title="Toggle dark mode"		
-          >		
-            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}		
-          </a>		
-        )}		
-      </div>		
-
-      <div className={styles.social}>		
-        <a		
-          className={styles.rss}		
-          href="/feed.xml"		
-          target="_blank"		
-          rel="noopener noreferrer"		
-        >		
-          <FaRss />		
-        </a>		
-        {config.twitter && (		
-          <a		
-            className={styles.twitter}		
-            href={`https://twitter.com/${config.twitter}`}		
-            title={`Twitter @${config.twitter}`}		
-            target="_blank"		
-            rel="noopener noreferrer"		
-          >		
-            <FaTwitter />		
-          </a>		
-        )}		
-
-        {config.mastodon && (		
-          <a		
-            className={styles.mastodon}		
-            href={config.mastodon}		
-            title={`Mastodon ${config.getMastodonHandle()}`}		
-            rel="me"		
-          >		
-            <FaMastodon />		
-          </a>		
-    )}
-
-    {config.github && (		
-      <a		
-        className={styles.github}		
-        href={`https://github.com/${config.github}`}		
-        title={`GitHub @${config.github}`}		
-        target="_blank"		
-        rel="noopener noreferrer"		
-      >		
-        <FaGithub />		
-      </a>		
-    )}		
-
-    {config.linkedin && (		
-      <a		
-        className={styles.linkedin}		
-        href={`https://www.linkedin.com/in/${config.linkedin}`}		
-        title={`LinkedIn ${config.author}`}		
-        target="_blank"		
-        rel="noopener noreferrer"		
-      >		
-        <FaLinkedin />		
-      </a>		
-    )}		
-
-    {config.newsletter && (		
-      <a		
-        className={styles.newsletter}		
-        href={`${config.newsletter}`}		
-        title={`Newsletter ${config.author}`}		
-        target="_blank"		
-        rel="noopener noreferrer"		
-      >		
-        <FaEnvelopeOpenText />		
-      </a>		
-    )}		
-
-    {config.youtube && (		
-      <a		
-        className={styles.youtube}		
-        href={`https://www.youtube.com/${config.youtube}`}		
-        title={`YouTube ${config.author}`}		
-        target="_blank"		
-        rel="noopener noreferrer"		
-      >		
-        <FaYoutube />		
-      </a>		
-    )}		
-  </div>		
-</footer>		
-);
-};
-
-export default Footer;
+export default Footer
